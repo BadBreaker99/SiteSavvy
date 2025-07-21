2e5d709c089a1475afd8aa6d3c8bd52d42820d08
Panosmark6 <panosmark6@gmail.com>
2025-07-21 14:47:45 +0300

    0.03v

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const templates = require('./data/templates');
 require('dotenv').config();
 const sendAppointment = require('./routes/send-appointment');
-
+const newsletter = true;
 
 const ejsMate = require('ejs-mate'); // ✅ αυτό έλειπε
 
@@ -18,13 +18,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.get('/', (req, res) => res.render('index', { title: 'Αρχική' }));
 app.get('/templates', (req, res) => res.render('templates', { title: 'Templates', templates }));
-app.get('/pricing', (req, res) => res.render('pricing', { title: 'Τιμές' }));
-app.get('/contact', (req, res) => res.render('contact', { title: 'Επικοινωνία' }));
+
 app.get('/preview/:template', (req, res) => {
   res.render(`previews/${req.params.template}`, {
     title: `Προεπισκόπηση ${req.params.template}`
   });
 });
+app.get('/newsletter', (req, res) => {
+  res.render('newsletter', { title: 'Newsletter' });
+});
+app.get('/privacy-policy', (req, res) => {
+  res.render('privacy-policy', { title: 'Πολιτική Απορρήτου' });
+});
+
 app.use(sendAppointment);
 
 const PORT = process.env.PORT || 3001;
